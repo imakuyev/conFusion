27ac5bb5fab6810743f2d30b21f1ed6f92da7c96
Igor Makuyev <igor.makuyev@bnymellon.com>
2018-08-10 12:26:48 -0400

    2.3 Services

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
-import { DISHES } from '../shared/dishes';
+//import { DISHES } from '../shared/dishes';
 
-
-
+import { DishService } from '../services/dish.service';
 
 
 @Component({
@@ -12,12 +11,15 @@ import { DISHES } from '../shared/dishes';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
-  dishes: Dish[]=DISHES;
+
+  dishes: Dish[];
   selectedDish:Dish; //= DISHES[0];
   
-  constructor() { }
+  //constructor() { }
+  constructor(private dishService: DishService) { }
 
   ngOnInit() {
+    this.dishes = this.dishService.getDishes();
   }
  
 
