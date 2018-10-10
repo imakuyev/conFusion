0c800f8f1bc641bdb885e2b2a8fba2247a8c0189
Igor Makuyev <igor.makuyev@bnymellon.com>
2018-10-10 16:10:29 -0400

    Animations Part 2B

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -5,13 +5,17 @@ import { Location } from '@angular/common';
 import { Leader } from '../shared/leader';
 import { LEADERS } from '../shared/leaders';
 import { LeaderService } from '../services/leader.service';
-
+import { flyInOut, expand } from '../animations/app.animation';
 
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  animations: [
+    flyInOut(),
+    expand()
+  ]
 })
 export class AboutComponent implements OnInit {
 
